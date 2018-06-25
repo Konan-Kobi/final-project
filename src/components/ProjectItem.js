83725ccd8499e848cb68328b67dfaab854514b04
koobohyeon <goosee115@gmail.com>
2018-06-25 17:28:18 +0900

    add project list presentation component

---
+++ src/components/ProjectItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class ProjectItem extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>title</div>
+        <div>1/10</div>
+      </div>
+    );
+  }
+}
---
+++ src/components/ProjectList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ProjectItem from '../components/ProjectItem';
+export default class ProjectList extends React.Component {
+  render() {
+    return <ProjectItem />;
+  }
+}
