21cda90634b3a7cbd4ff350f350b2db155001bc9
leekeunhwan <dlrmsghks7@gmail.com>
2018-07-03 15:25:35 +0900

    solve conflict

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -36,8 +36,7 @@ export default class MyPage extends React.Component {
                           <ProjectListContainer />
                         </Grid.Column>
                         <Grid.Column>
-                          <h1>나의 이슈 리스트gg</h1>
-
+                          <h1>나의 이슈 리스트</h1>
                           <IssueListContainer />
                         </Grid.Column>
                       </Grid.Row>
